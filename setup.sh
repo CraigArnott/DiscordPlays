3344cb882dd20f4b7d5c14358956ca6a1c495622
CraigArnott <arnottc@tcd.ie>
2017-04-29 00:04:40 +0100

    add autogui dependency

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 sudo apt-get install python3 python3-pip -y
 
 #install dependencies
-sudo python3 -m pip install -U discord.py pyyaml
+sudo python3 -m pip install -U python3-Xlib
+sudo python3 -m pip install -U discord.py pyyaml pyautogui 
